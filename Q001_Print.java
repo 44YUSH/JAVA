11c6a3bc6e1450f8ad5e0b949b5a206a5ab12549
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-09 21:39:29 +0530

    .

--- Q001_Print.java
+++ Q001_Print.java
@@ -1,4 +1,4 @@
-public class Q0001{
+public class Q001_Print{
     public static void main(String[] args) {
         System.out.println("HELLO Aayush");
         System.out.println("*\n**\n***\n****\n*****");
---
+++ Q002_PrintZ.java
@@ -0,0 +1,5 @@
+public class Q002_PrintZ {
+    public static void main(String[] args) {
+        System.out.println("It's a Z :\n \n*****\n   *\n  *\n *\n*****" );
+    }
+}
