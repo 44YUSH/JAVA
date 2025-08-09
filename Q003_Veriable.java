3ca686a7de4a8208220447726a2a399ded42ca25
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-09 21:52:06 +0530

    .

--- Q003_Veriable.java
+++ Q003_Veriable.java
@@ -1,8 +1,19 @@
 public class Q003_Veriable {
     public static void main(String[] args) {
+        //SUM
         int a = 10;
         int b = 20;
         int sum = a + b ;
         System.out.println("Sum is : "+sum);
+
+        //Product
+        int prod = a * b;
+        System.out.println("Product is : "+prod);
+
+        // Expression
+        int exp1 = a * b / a + b;
+        int exp2 = (a * b)/(a + b);
+        System.out.println("Exp 1 : "+exp1);
+        System.out.println("Exp 2 : "+exp2);
     }
 }
