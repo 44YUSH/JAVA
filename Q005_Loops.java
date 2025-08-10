959bdb46255bd6b05043962d4e135e8e9af43951
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-11 01:12:49 +0530

    .

---
+++ Q005_Loops.java
@@ -0,0 +1,10 @@
+public class Q005_Loops {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while(i < 11){
+            System.out.print(i +"  ");
+            i++;
+        }
+    }
+}
