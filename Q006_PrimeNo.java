70a897da289bd6cd60873ff7e52b1605355d7be2
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-11 21:49:47 +0530

    .

---
+++ Q006_PrimeNo.java
@@ -0,0 +1,35 @@
+/*
+PRIME NUMBER
+
+If P & Q are two nos. and (PQ = n) ,
+then, P !> root(n) and Q !> root(n).
+ */
+
+import java.util.*;
+public class Q006_PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no : ");
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++){
+            int n = sc.nextInt();
+
+            int count = 0;
+            for(int div = 2; div * div <= n; div++){
+                if(n % div ==0){
+                    count++;
+                    break;
+                }
+            }
+
+            if(count == 0){
+                System.out.println("PRIME");
+            }
+            else{
+                System.out.println("NOT PRIME");
+            }
+        }
+    }
+}
