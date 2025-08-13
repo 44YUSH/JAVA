c87e3fbfa605048b2401e26fa7dd90bf27f6ace5
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-13 11:31:23 +0530

    .

--- Q019_Pattern_2.java
+++ Q019_Pattern_2.java
@@ -18,7 +18,7 @@ public class Q019_Pattern_2 {
         int n = sc.nextInt();
 
         System.out.println("\nYour pattern : \n");
-        for(int i = n; i > 0; i--){
+        for(int i = n; i >= 1; i--){
             for(int j = 1; j <= i; j++){
                 System.out.print("* ");
             }
