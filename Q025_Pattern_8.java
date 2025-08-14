d01c086f7b4b994c30d3ac242bcf270877669eec
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-14 20:49:08 +0530

    .

---
+++ Q025_Pattern_8.java
@@ -0,0 +1,29 @@
+/*
+Pattern 7 : 
+
+        *
+      *
+    *
+  *
+* 
+
+ */
+
+import java.util.*;
+public class Q025_Pattern_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no : ");
+        int n = sc.nextInt();
+
+        System.out.println("\nYour pattern : \n");
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j < i; j++){
+                System.out.print("  ");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
