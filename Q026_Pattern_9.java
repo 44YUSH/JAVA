bdf2f24e14cd60f54c14625168036be8a1391e46
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-16 12:11:18 +0530

    .

--- Q026_Pattern_9.java
+++ Q026_Pattern_9.java
@@ -1,11 +1,11 @@
 /*
 Pattern 9 : 
 
-        *
-      *
-    *
-  *
-* 
+*       * 
+  *   *   
+    *     
+  *   *   
+*       * 
 
  */
 
@@ -21,8 +21,15 @@ public class Q026_Pattern_9 {
 
         int space = n-2;
         for(int i = 1; i <= n; i++){
-            
-            System.out.println();
+          for(int j = 1; j <= n; j++){
+            if(i == j || i + j == n + 1){
+              System.out.print("* ");
+            }
+            else{
+              System.out.print("  ");
+            }
+          }
+          System.out.println();
         }
     }
 }
