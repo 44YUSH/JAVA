a21f02909c2b3bb7647fe543b2ac2310e17b54bf
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-16 13:24:02 +0530

    .

--- Q008_Fibonacci.java
+++ Q008_Fibonacci.java
@@ -10,7 +10,7 @@ public class Q008_Fibonacci {
         int a = 0;
         int b = 1;
         for(int i=0; i<n; i++){
-            System.out.println(a);
+            System.out.print(a+"  ");
             int c = a + b;
             a = b;
             b = c;
