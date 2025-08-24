3358a33653f7ece780ca10785a4faa7ffcd94a82
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-08-25 00:04:24 +0530

    .

---
+++ Q034_NumPattern_17.java
@@ -0,0 +1,54 @@
+/*
+Pattern 17 : 
+
+    * 
+    * * 
+* * * * * 
+    * * 
+    *    
+
+ */
+
+import java.util.*;
+public class Q034_NumPattern_17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no : ");
+        int n = sc.nextInt();
+
+        System.out.println("\nYour pattern : \n");
+
+        int space1 = n/2;
+        int star = 1;
+        int space2 = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= space1; j++){
+                if(i == (n/2)+1){
+                    /* SKIP */
+                }
+                else{
+                    System.out.print("  ");
+                }  
+            }
+            if(i == (n/2)+1){
+                for(int k = 1; k <= n; k++){
+                System.out.print("* ");
+                }
+            }
+            else{
+                for(int k = 1; k <= star; k++){
+                    System.out.print("* ");
+                }
+            }
+
+            if(i <= n/2){
+                star++;
+            }
+            else{
+                star--;
+            }
+            System.out.println();
+        }
+    }
+}
