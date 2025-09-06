98295b5f9ae7f771eac502373c1b32793d2df5ff
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-09-06 23:02:48 +0530

    .

--- Q050_NextBigElementToRightUsingStack.java
+++ Q050_NextBigElementToRightUsingStack.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Q051_NextBigElementToRightUsingStack {
+public class Q050_NextBigElementToRightUsingStack {
 
     public static void display(int[] a) {
         StringBuilder sb = new StringBuilder();
