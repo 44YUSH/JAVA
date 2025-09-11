a9eac2527edc0f24bdf8ac3046cb628b800ae83b
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-09-11 11:13:26 +0530

    .

--- OPPs/Constructor/JustForFun.java
+++ OPPs/Constructor/JustForFun.java
@@ -1,5 +1,6 @@
-package Constructor;
-
 public class JustForFun {
-    
+    public static void main(String[] args) {
+        Student s1 = new Student("Aayush", 21);
+        System.out.println(s1);
+    }
 }
--- OPPs/Constructor/Student.java
+++ OPPs/Constructor/Student.java
@@ -1,3 +1,10 @@
 public class Student {
-    
+    Student(String s1, int y){
+        String name = s1;
+        int yob = y;
+    }
+
+    Student(Student s1){
+        return;
+    }
 }
