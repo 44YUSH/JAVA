9d684d06fb22c0c412f33abdf5a6c379662c2a41
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-09-14 00:41:31 +0530

    .

---
+++ Q53_SlidingWindowMax.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Q53_SlidingWindowMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length : ");
+        int l = sc.nextInt();
+        int arr[] = new int[l];
+        System.out.println("Enter the elements : ");
+        for (int i = 0; i < l; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the window size (K) : ");
+        int k = sc.nextInt();
+
+        int res[] = new int[l - k + 1];
+
+        for (int start = 0; start <= l - k; start++) {
+            Stack<Integer> st = new Stack<>();
+            int max = arr[start];
+
+            // push all elements of current window
+            for (int i = start; i < start + k; i++) {
+                st.push(arr[i]);
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+
+            res[start] = max;
+        }
+
+        // Print results
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+}
