32c29e0a060b48c3d6f2a851208ee59d4d5397d7
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-09-13 12:21:10 +0530

    .

---
+++ Q051_StockSpan_LeftBigElement.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Q051_StockSpan_LeftBigElement {
+
+    // DISPLAY
+    public static void display(int[] a) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : a) {
+            sb.append(val + " ");
+        }
+        System.out.println(sb);
+    }
+
+    // SOLVE
+    public static int[] solve(int[] arr){
+        int span[] = new int[arr.length];
+
+        Stack <Integer> st = new Stack<>();
+        st.push(0);
+        span[0] = 1;
+        for(int i = 1; i < arr.length; i++){
+            while(st.size() > 0 && arr[i] > arr[st.peek()]){
+                st.pop();
+            }
+
+            if(st.size() == 0){
+                span[i] = i+1;
+            }
+            else{
+                span[i] = i -st.peek();
+            }
+            st.push(i);
+        }
+
+        return span;
+    }
+
+    // MAIN
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter Span Length : ");
+        int n = Integer.parseInt(br.readLine());
+        int a[] = new int[n];
+        System.out.println("Enter Span Values : ");
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        int span[] = solve(a);
+        System.out.println("Stock Span : ");
+        display(span);
+    }
+}
