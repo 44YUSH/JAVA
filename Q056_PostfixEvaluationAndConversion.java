db3cdaaa4e876a6db3ec309fe6f45f0801004180
A.a.y.u.s.h <aayushsith0@gmail.com>
2025-10-08 21:17:16 +0530

    .

--- Q056_PostfixEvaluationAndConversion.java
+++ Q056_PostfixEvaluationAndConversion.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 public class Q056_PostfixEvaluationAndConversion {
     public static void main(String[] args) throws IOException {
 
@@ -8,7 +9,52 @@ public class Q056_PostfixEvaluationAndConversion {
         System.out.print("Enter Postfix expression: ");
         String exp = br.readLine();
 
-        
+        Stack<Integer> vs = new Stack<>();
+        Stack<String>  is = new Stack<>();
+        Stack<String>  ps = new Stack<>();
 
+        for(int i = 0; i < exp.length(); i++){
+            char ch = exp.charAt(i);
+
+            if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+                int v2 = vs.pop();
+                int v1 = vs.pop();
+                int val= operation(v1, v2, ch);
+                vs.push(val);
+
+                String iv2 = is.pop();
+                String iv1 = is.pop();
+                String ival = "(" + iv1 + ch + iv2 + ")";
+                is.push(ival);
+
+                String pv2 = ps.pop();
+                String pv1 = ps.pop();
+                String pval = ch + pv1 + pv2;
+                ps.push(pval);
+            }
+            else{
+                vs.push(ch - '0');
+                is.push(ch + "");
+                ps.push(ch + "");
+            }
+        }
+        System.out.println("Value :  "+vs.pop());
+        System.out.println("Infix :  "+is.pop());
+        System.out.println("Prefix : "+ps.pop());
+    }
+
+    public static int operation(int v1, int v2, char op){
+        if(op == '+'){
+            return v1 + v2;
+        }
+        else if(op == '-'){
+            return v1 - v2;
+        }
+        else if(op == '*'){
+            return v1 * v2;
+        }
+        else{
+            return v1 / v2;
+        }
     }
 }
